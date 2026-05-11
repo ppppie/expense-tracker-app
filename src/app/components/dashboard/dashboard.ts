45cee6244ef52dc3ccabb0e69c70358f0bb318dc
Maddie Taghon <thessa.taghon@ndsu.edu>
2026-05-11 05:05:23 -0500

    Added pie chart

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,12 +1,50 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
 import { ExpenseService } from '../../services/expense';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartData, ChartOptions } from 'chart.js';
+import { Chart, PieController, ArcElement, Tooltip, Legend } from 'chart.js';
+
+Chart.register(PieController, ArcElement, Tooltip, Legend);
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  pieChartData = computed<ChartData<'pie'>>(() => ({
+    labels: this.expenseService.expenseCategoryTotals().map((category) => category[0]),
+
+    datasets: [
+      {
+        data: this.expenseService.expenseCategoryTotals().map((category) => category[1]),
+
+        backgroundColor: [
+          '#0d6efd',
+          '#ffc107',
+          '#dc3545',
+          '#0dcaf0',
+          '#6f42c1',
+          '#fd7e14',
+          '#6c757d',
+          '#198754',
+        ],
+
+        borderWidth: 2,
+      },
+    ],
+  }));
+
+  pieChartOptions: ChartOptions<'pie'> = {
+    responsive: true,
+
+    plugins: {
+      legend: {
+        position: 'bottom',
+      },
+    },
+  };
+
   expenseService = inject(ExpenseService);
 }
